e67a8540fbfebeadfa4a045b3842e7cd7439185f
ayush893 <mauryaayush893@gmail.com>
2021-10-19 22:24:57 +0530

    Mobile responsive and exchaange completed

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+ 
+import 'antd/dist/antd.css';
+import store from './app/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Router>
+        <Provider store={store}>
+          <App/>
+        </Provider>
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,38 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'b626f9c1f4mshf040b7e2c54cb34p139cf1jsn4ac03566daf4'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ( { url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        }),
+        getExchanges: builder.query({
+            query: () => createRequest('/exchanges')
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`)
+        }),
+        getCryptoHistory: builder.query({
+            query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`),
+          }),
+    })
+});
+
+//redux toolkit create hooks that you can call to get all data instantly, give all state like loading etc
+
+export const {
+    useGetCryptosQuery,
+    useGetExchangesQuery,
+    useGetCryptoDetailsQuery,
+    useGetCryptoHistoryQuery,
+} = cryptoApi;
